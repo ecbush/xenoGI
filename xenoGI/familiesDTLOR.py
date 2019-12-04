719d70e0e181449d35f87397348a56979869bff2
Ivy Liu <ivliu@cs.hmc.edu>
2019-12-03 16:47:37 -0800

    implemented and tested another way to separate initial family into locus families

--- xenoGI/familiesDTLOR.py
+++ xenoGI/familiesDTLOR.py
@@ -1,8 +1,11 @@
+
 #import sys,numpy,random
 #from scipy.signal import find_peaks
-import scores
-from Family import *
-from analysis import printTable
+import os
+from .Family import *
+from . import scores
+from .families import *
+from .analysis import printTable
 import random
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,10 +21,13 @@ sys.setrecursionlimit(10000)
 
 #### Main function
 
-def createFamiliesO(tree,scoresO,genesO, method="affinity"):
+def createDTLORFamiliesO(tree,scoresO,genesO,aabrhHardCoreL,paramD,method="threshold"):
     '''Given a scoresO object, create gene families using the DTLOR approach.
     '''
+    synThresholdD = getSynThresholdD(paramD,scoresO,genesO,aabrhHardCoreL,tree)
     initial_families=createInitialFamily(scoresO)
+    print("The number of initial families is: ")
+    print(len(initial_families))
     locusMap={}
     #construct Family object for the entire problem
     familiesO = Families(tree)
@@ -34,8 +40,8 @@ def createFamiliesO(tree,scoresO,genesO, method="affinity"):
 
         mrca=findMRCA(species, tree)
         familiesO.initializeFamily(index,mrca,seedPairL=None) 
-
-        clustered_fam=clusterLocusFamily(family, scoresO, method)
+        #(family, genesO, scoresO, paramD,synThresholdD,method,min_gene=2, max_cluster=5):
+        clustered_fam=clusterLocusFamily(family, genesO,scoresO,paramD,synThresholdD,method)
         for locusFamily in clustered_fam:
             species=[]
             for gene in locusFamily:
@@ -45,11 +51,13 @@ def createFamiliesO(tree,scoresO,genesO, method="affinity"):
             lf=LocusFamily(index,locusFamId,lfMrca)
             familiesO.addLocusFamily(lf)
             locusFamId+=1
-        # species=[genesO.numToStrainName(gene) for gene in genes]
-        #create families object for the initial family
+        #how many locus families are there in this initial family
         distribution.append(len(clustered_fam))
+    print("The number of locus families is: ")
+    print(locusFamId)
        
     distribution=Counter(distribution)
+    print("Here is the distribution of the number of locus families each initial family has:")
     print(distribution)
     return familiesO, locusMap
 
@@ -118,10 +126,6 @@ def createInitialFamily(scoresO):
   
        
         visited.add(gene)
-        print("add %d to visited"%gene)
-        print(len(visited))
-        
- 
         temp.add(gene) 
   
         # Repeat for all vertices adjacent 
@@ -140,7 +144,6 @@ def createInitialFamily(scoresO):
     
     initFamily=[]
     visited=set()
-    print(len(visited))
     for gene in connectedGenes: 
         if gene not in visited: 
             temp =set()
@@ -178,8 +181,42 @@ def consensusEigens(vals1,vecs1, vals2, vecs2):
             vecs[:,i]=vecs2[:,i]
         
     return vals, vecs
+
+#This function is modified from isSameLocusFamily() in families.py
+def isSameLocus(gene1,gene2,scoresO,genesO,paramD,synThresholdD):
     
-def clusterLocusFamily(family, scoresO, method="spectral",min_gene=2, max_cluster=5):
+    
+    coreSynSc = scoresO.getScoreByEndNodes(gene1,gene2,'coreSynSc')
+    synSc = scoresO.getScoreByEndNodes(gene1,gene2,'synSc')
+    euclid_score=math.sqrt(coreSynSc**2+synSc**2)/math.sqrt(2)
+    if not scoresO.isEdgePresentByEndNodes(gene1,gene2):
+        # Within our families, there may be some gene-gene edges
+        # missing due to the fact that blast could have just missed
+        # significance etc. If the edge isn't there, then we do not
+        # have evidence that these genes should be in the same locus
+        # family, and we return false.
+        
+        # NOTE: An alternative approach would be to actually calculate
+        # these scores here. But they're likely to be low...
+
+        return (False, euclid_score)
+    strain1 = genesO.numToStrainName(gene1)
+    strain2 = genesO.numToStrainName(gene2)
+    strainPair = tuple(sorted([strain1,strain2]))
+    minSynThreshold = synThresholdD['minSynThreshold'][strainPair]
+    minCoreSynThreshold = synThresholdD['minCoreSynThreshold'][strainPair]
+    
+    if coreSynSc < minCoreSynThreshold or synSc < minSynThreshold:
+        # one of the two types of synteny below threshold, so this
+        # pair doesn't meet the requirements for being in the same
+        # LocusFamily
+        addIt = False
+    else:
+        addIt = True
+            
+    return (addIt, euclid_score)
+    
+def clusterLocusFamily(family, genesO, scoresO, paramD,synThresholdD,method,min_gene=2, max_cluster=5):
     """
     Input 
     ----------------------------------------
@@ -201,26 +238,36 @@ def clusterLocusFamily(family, scoresO, method="spectral",min_gene=2, max_cluste
     #construct the fully connected graph with weighted edges 
     num_gene=len(family)
     genes=np.array(list(family))
+    gene_neighbors={}
     #note there is no edge going from one to self.
     if method=="spectral":
         graph=np.zeros((num_gene,num_gene))
     if method=="affinity":
         graph=np.ones((num_gene,num_gene))
+    #loop over all pairs of genes
     for i in range(len(genes)):
         for j in range(i+1, len(genes)):
             gene1=genes[i]
             gene2=genes[j]
             if scoresO.isEdgePresentByEndNodes(gene1,gene2):
-              
-                coreSynSc = scoresO.getScoreByEndNodes(gene1,gene2,'coreSynSc')
-                synSc = scoresO.getScoreByEndNodes(gene1,gene2,'synSc')
-            
-                euclid_score=math.sqrt(coreSynSc**2+synSc**2)/math.sqrt(2)
-        
-           
+                sameLocus, euclid_score=isSameLocus(gene1,gene2,scoresO,genesO,paramD,synThresholdD)
                 #the graph is symmetrical
-                graph[i][j]=euclid_score
-                graph[j][i]=euclid_score
+                if method=="affinity" or method=="spectral":
+                    graph[i][j]=euclid_score
+                    graph[j][i]=euclid_score
+                if method=="threshold":
+                    #initialize the adjacency dictionary if key not already exists
+                    if gene1 not in gene_neighbors:
+                        gene_neighbors[gene1]=[]
+                    if gene2 not in gene_neighbors:
+                        gene_neighbors[gene2]=[]
+                    #if they are above threshold for both synteny scores, add to each other's neighbors
+                    if sameLocus:
+                        # graph[i][j]=1
+                        # graph[j][i]=1
+                        gene_neighbors[gene1].append(gene2)
+                        gene_neighbors[gene2].append(gene1)
+
 
     
     if method=="spectral":
@@ -256,15 +303,37 @@ def clusterLocusFamily(family, scoresO, method="spectral",min_gene=2, max_cluste
         if cluster_labels[0]==-1:
             #the algorithm did not converge
             cluster_labels=np.arange(len(genes))
-    num_clusters=len(np.unique(cluster_labels))
-
     clustered_fam=[]
-    for i in range(num_clusters):
-        #get the index of gene in that cluster
-        indices=np.argwhere(cluster_labels==i)
-        #get the actual index associated with each gene
-        fam=[genes[index[0]] for index in indices]
-        clustered_fam.append(fam)
+    if method!="threshold":
+        num_clusters=len(np.unique(cluster_labels))
+        for i in range(num_clusters):
+            #get the index of gene in that cluster
+            indices=np.argwhere(cluster_labels==i)
+            #get the actual index associated with each gene
+            fam=[genes[index[0]] for index in indices]
+            clustered_fam.append(fam)
+
+    if method=="threshold":
+        #find connected components
+        def stronglyConnected(temp, gene, visited): 
+            visited.add(gene)
+            temp.append(gene)
+            neighbors=gene_neighbors[gene]
+            if neighbors:
+                for i in neighbors: 
+                    if i not in visited:     
+                        # Update the list 
+                        temp = stronglyConnected(temp, i, visited) 
+            return temp 
+        #end of helper
+        visited=set()
+        for gene in genes: 
+            if gene not in visited: 
+                temp =[]
+                newFam=stronglyConnected(temp, gene, visited)
+                clustered_fam.append(newFam) 
+
+    
 
     return clustered_fam
 
@@ -274,53 +343,53 @@ def clusterLocusFamily(family, scoresO, method="spectral",min_gene=2, max_cluste
     
 ## Input/output
 
-def writeFamilies(familiesO,genesO,strainNamesT,paramD):
-    '''Write all gene families to fileName, one family per line.'''
+# def writeFamilies(familiesO,genesO,strainNamesT,paramD):
+#     '''Write all gene families to fileName, one family per line.'''
 
-    familyFN = paramD['familyFN']
-    geneInfoFN = paramD['geneInfoFN']
+#     familyFN = paramD['familyFN']
+#     geneInfoFN = paramD['geneInfoFN']
 
-    # get the num to name dict, only for strains we're looking at.
-    genesO.initializeGeneNumToNameD(geneInfoFN,set(strainNamesT))
+#     # get the num to name dict, only for strains we're looking at.
+#     genesO.initializeGeneNumToNameD(geneInfoFN,set(strainNamesT))
     
-    f=open(familyFN,'w')
-    for fam in familiesO.iterFamilies():
-        f.write(fam.fileStr(genesO)+'\n')
-    f.close()
+#     f=open(familyFN,'w')
+#     for fam in familiesO.iterFamilies():
+#         f.write(fam.fileStr(genesO)+'\n')
+#     f.close()
 
 
-def readFamilies(familyFN,tree,genesO):
-    '''Read the family file named familyFN, creating a Families object.
-    '''
-    familiesO = Families(tree)
-    f=open(familyFN,'r')
-    while True:
-        s=f.readline()
-        if s=='':
-            break
-        L=s.split('\t')
-        famNum=int(L[0])
-        mrca = L[1]
-        if L[2] == "-":
-            seedPairL = None
-        else:
-            seedPairL = [L[2],L[3]]
+# def readFamilies(familyFN,tree,genesO):
+#     '''Read the family file named familyFN, creating a Families object.
+#     '''
+#     familiesO = Families(tree)
+#     f=open(familyFN,'r')
+#     while True:
+#         s=f.readline()
+#         if s=='':
+#             break
+#         L=s.split('\t')
+#         famNum=int(L[0])
+#         mrca = L[1]
+#         if L[2] == "-":
+#             seedPairL = None
+#         else:
+#             seedPairL = [L[2],L[3]]
 
-        lfL = L[4:]
+#         lfL = L[4:]
 
-        familiesO.initializeFamily(famNum,mrca,seedPairL)
+#         familiesO.initializeFamily(famNum,mrca,seedPairL)
 
-        for lfStr in lfL:
-            lfSplitL = lfStr.rstrip().split(',')
-            locusFamNum=int(lfSplitL[0])
-            lfMrca = lfSplitL[1]
-            geneL=[]
-            for geneName in lfSplitL[2:]:
-                geneNum = int(geneName.split('_')[0])
-                geneL.append(geneNum)
-            lfO = LocusFamily(famNum,locusFamNum,lfMrca)
-            lfO.addGenes(geneL,genesO)
-            familiesO.addLocusFamily(lfO)
+#         for lfStr in lfL:
+#             lfSplitL = lfStr.rstrip().split(',')
+#             locusFamNum=int(lfSplitL[0])
+#             lfMrca = lfSplitL[1]
+#             geneL=[]
+#             for geneName in lfSplitL[2:]:
+#                 geneNum = int(geneName.split('_')[0])
+#                 geneL.append(geneNum)
+#             lfO = LocusFamily(famNum,locusFamNum,lfMrca)
+#             lfO.addGenes(geneL,genesO)
+#             familiesO.addLocusFamily(lfO)
 
-    f.close()
-    return familiesO
+#     f.close()
+#     return familiesO
